6fde86ed4bf366c449c90539f62dc1a7e45a8ac7
princeakhoury13 <princepraveen130509@gmail.com>
2023-03-11 19:07:33 +0530

    added: FILE IO Operations to AddressBook

--- Program.cs
+++ Program.cs
@@ -4,7 +4,22 @@
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to the master branch of Assignment: Day 27 Address Book File IO");
+            AddressBook myAddressBook = new AddressBook();
+            Contacts newContact = new Contacts("Prince", "Praveen", "INDIA", "8369319177", "prince.p@example.com");
+            Contacts newContact2 = new Contacts("Robert", "Downey", "USA", "888222444", "robery.downey@jr.com");
+            //Contacts newContact3 = new Contacts("Robert", "Downey", "USA", "888222444", "robery.downey@jr.com");
+
+            myAddressBook.AddContact(newContact);
+            myAddressBook.AddContact(newContact2);
+            
+            myAddressBook.PrintContacts();
+
+
+            string path = "C:\\Users\\princ\\OneDrive\\Desktop\\dotnet\\Day27AddressBookFileIO\\Day27AddressBookFileIO\\NewFile.txt";
+
+
+            //myAddressBook.WriteToFile(path);
+            //myAddressBook.ReadFromFile(path);
         }
-    }
-}
+     }
+ }
